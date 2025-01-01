c96e32c75091ebf369ebfbc2637e7f8b8744bf37
bernhard-franzl <bernhard.franzl@gmx.at>
2025-01-01 02:52:13 +0100

    new stuff

--- forecasting/test_on_occupancy_data.py
+++ forecasting/test_on_occupancy_data.py
@@ -6,23 +6,23 @@ import torch
 ##############################
 # Run n tests
 #cp_log_dir = "_occupancy_forecasting/checkpoints/occrate"
-cp_log_dir = "/home/berni/transfer_from_server/wrap_up_combs_1"
+cp_log_dir = "/home/franzl/transfer_from_server/wrap_up_combs_2"
 
 for mode in ["normal"]:
-    for run_id in [4]:
+    for run_id in [0,1,2,3,4]:
    
         test_suite = OccupancyTestSuite(
             cp_log_dir=cp_log_dir,
             path_to_data="data/occupancy_forecasting",
             path_to_helpers="data/occupancy_forecasting",
-            path_to_results=f"results_wrapup_{mode}_combs.txt",
+            path_to_results=f"results_wrapup_{mode}_combs_laptop.txt",
             #path_to_results=f"results_{mode}.txt",
             erase_results_file=False,
         )
         tuples_run_comb = sorted(test_suite.list_checkpoints(run_id))
 
         test_suite.evaluate_combinations(
-            comb_tuples=tuples_run_comb[:200],
+            comb_tuples=tuples_run_comb,
             print_results=False,
             plot_results=False,
             dataset_mode=mode,
